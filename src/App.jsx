d0fde7eb7e0dbfd64c802bd105812e065af50726
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-21 20:57:15 +0100

    Navbar added in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './styles/App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from './pages/home';
 import { NotFound } from './pages/notFound';
+import { NavBar } from './components/nav/NavBar';
 
 function App() {
 
   return (
     
     <div className="App">
+      <NavBar/>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}/>
