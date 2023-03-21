19dfbcc47a4643d461816cc6921826d8baea7052
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-21 15:11:56 +0100

    notFound route created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './styles/App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from './pages/home';
+import { NotFound } from './pages/notFound';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
     </div>
