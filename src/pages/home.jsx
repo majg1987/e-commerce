089e70508117b40bf0a4973b0c715f2915690a28
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-22 20:37:07 +0100

    Portada component added in  page home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Portada } from '../components/home/Portada'
 
 export const Home = () => {
+  
   return (
-    <div>Home</div>
+    <div className=''>
+      <Portada/>
+    </div>
   )
 }
