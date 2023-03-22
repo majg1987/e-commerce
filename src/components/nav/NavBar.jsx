2000e1ee34c11f6c981560b41657c5d4838a4bfe
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-22 20:36:29 +0100

    basic NavBar layout

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faUser } from '@fortawesome/free-solid-svg-icons'
 
 export const NavBar = () => {
   return (
-    <div className='h-16  bg-color1'>
-
+    <div className='h-14 bg-color2 flex justify-between items-center'>
+      <div>
+        <FontAwesomeIcon icon={faHouse} className='text-color5 w-8 h-8 ml-8'/>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faUser} className='text-color5 w-8 h-8 mr-8'/>
+      </div>
     </div>
   )
 }
