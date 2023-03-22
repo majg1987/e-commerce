d5603056ff98a54f91be054964c3698b6d3dc258
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-22 12:00:28 +0100

    footer added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from './pages/home';
 import { NotFound } from './pages/notFound';
 import { NavBar } from './components/nav/NavBar';
+import { Footer } from './components/footer/Footer';
 
 function App() {
 
   return (
     
-    <div className="App">
-      <NavBar/>
+    <div className="App relative h-screen">  
       <BrowserRouter>
+        <NavBar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   )
