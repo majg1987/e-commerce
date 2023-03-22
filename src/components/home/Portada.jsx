753041d6f1eba5f6461529bd35d1eb6bdee25d1c
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-22 15:19:45 +0100

    portada component layout

---
+++ src/components/home/Portada.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../styles/portada.css'
+
+export const Portada = () => {
+  return (
+    <>
+        <div className='portada1 flex justify-center items-stretch py-5 mt-10'>
+            <p className='font-black text-4xl text-color2 self-center uppercase'>
+                Los juguetes que no quieras...
+            </p>
+        </div>
+        <div className='portada flex justify-center items-stretch py-5'>
+            <p className='font-black text-4xl text-color2 self-center uppercase'>
+                ¡¡¡Vendelos!!!
+            </p>            
+        </div>
+    </>
+  )
+}
