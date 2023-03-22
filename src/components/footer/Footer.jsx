a661026dfeb79b6348215a4f505c5d104e35b5c7
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-22 11:59:04 +0100

    footer layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='h-16  bg-color1'>
-
+    <div className='absolute bottom-0 left-0 right-0 h-16 bg-color2 flex items-stretch justify-center '>
+      <p className='text-color5 font-black self-center'>© Todos los derechos reservados</p>
     </div>
   )
 }
