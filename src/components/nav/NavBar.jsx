68ee1e472c8f2662213f1a5f0739c5e57ae9122f
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-27 14:55:36 +0200

    navigation to login and home

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faUser } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
 
 export const NavBar = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className='h-14 bg-color2 flex justify-between items-center'>
       <div>
-        <FontAwesomeIcon icon={faHouse} className='text-color5 w-8 h-8 ml-8'/>
+        <FontAwesomeIcon onClick={() => navigate('/')} icon={faHouse} className='cursor-pointer text-color5 w-8 h-8 ml-8'/>
       </div>
       <div>
-        <FontAwesomeIcon icon={faUser} className='text-color5 w-8 h-8 mr-8'/>
+        <FontAwesomeIcon onClick={() => navigate('/login')} icon={faUser} className='cursor-pointer text-color5 w-8 h-8 mr-8'/>
       </div>
     </div>
   )
