6e78a0bcde1bb3ebe5e617e05833cdf17baf08e1
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-27 17:36:15 +0200

    portada component update

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -5,7 +5,7 @@ export const Portada = () => {
   return (
     <>
         <div className='portada1 flex justify-center items-stretch py-5'>
-            <p className='font-black text-4xl text-color2 self-center uppercase'>
+            <p className='text-center font-black text-4xl text-color2 self-center uppercase'>
                 Los juguetes que no quieras...
             </p>
         </div>
