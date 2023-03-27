acbcbd4df7887214966bb73444da09141a3f6756
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-27 17:28:56 +0200

    formRegister component added in registerUser page

--- src/pages/registerUser.jsx
+++ src/pages/registerUser.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { FormRegister } from '../components/forms/FormRegister'
 import { Portada } from '../components/Portada'
 
 export const RegisterUser = () => {
   return (
     <div>
         <Portada/>
-        
+        <FormRegister/>
     </div>
   )
 }
