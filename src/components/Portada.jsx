3d17c8be7f7a168feeef566d25e6dda622739d88
Miguel Angel Jurado <majg1987@hotmail.com>
2023-03-27 14:21:34 +0200

    portada component layout

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import '../../styles/portada.css'
+import '../styles/portada.css'
 
 export const Portada = () => {
   return (
     <>
-        <div className='portada1 flex justify-center items-stretch py-5 mt-10'>
+        <div className='portada1 flex justify-center items-stretch py-5'>
             <p className='font-black text-4xl text-color2 self-center uppercase'>
                 Los juguetes que no quieras...
             </p>
